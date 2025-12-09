e9a9cd4b4aaf7e22ab76daaaccc50c5882c24942
Jack Palevich <jackpal@google.com>
2025-12-08 23:38:48 -0800

    not working

--- 9.py
+++ 9.py
@@ -11,62 +11,48 @@ def solve(data: str, part: int) -> int:
   if part == 1:
     return max(area(a[0], a[1], b[0], b[1]) for a in tiles for b in tiles)
   elif part == 2:
-    x0, x1, y0, y1 = (min(a[0] for a in tiles), max(a[0] for a in tiles), min(a[1] for a in tiles), max(a[1] for a in tiles))
-    # dialate so flood fill works
-    x0 -= 1
-    x1 += 2
-    y0 -= 1
-    y1 += 2
     def arange(start: int, stop: int) -> iterator[int]:
       if start <= stop:
         return range(start, stop+1)
       else:
         return arange(stop, start)
 
-    def flood(grid: list[list[str]], x: int, y: int, src: str, dst: str):
-      active = set(((x, y),))
-      while active:
-        x, y = active.pop()
-        if not (x0 <= x < x1 and y0 <= y < y1):
-          continue
-        if grid[x-x0][y-y0] != src:
-          continue
-        grid[x-x0][y-y0] = dst
-        active.add((x+1, y))
-        active.add((x-1, y))
-        active.add((x, y+1))
-        active.add((x, y-1))
-    
-    def show(grid: list[list[str]]):
-      for row in grid:
-        print(''.join(row))
-    
-    def check(grid: list[list[str]], ax: int, ay: int, bx: int, by: int) -> int:
-      for x in arange(ax, bx):
-        for y in arange(ay, by):
-          if grid[x-x0][y-y0] == ' ':
-            return -1
-      return area(ax, ay, bx, by)
-    
-    grid = [['.'] * (y1 - y0) for _ in range(x1 - x0)]
-    prev = tiles[-1]
+    # Compressed grid
+    xs = sorted(set(a[0] for a in tiles))
+    ys = sorted(set(a[1] for a in tiles))
+    grid = [[0] * len(ys) for _ in xs]
+    prev_ix = xs.index(tiles[-1][0])
+    prev_iy = ys.index(tiles[-1][1])
     for x, y in tiles:
-      xp, yp = prev
-      for xx in arange(xp, x):
-        grid[xx - x0][yp - y0] = 'g'
-      for yy in arange(yp, y):
-        grid[x - x0][yy - y0] = 'g'
-      prev = (x, y)
-    # show(grid)
-    # print()
-    flood(grid, x0, y0, '.', ' ')
-    # show(grid)
+      curr_ix = xs.index(x)
+      curr_iy = ys.index(y)
+      if curr_ix != prev_ix:
+        for xx in arange(prev_ix, curr_ix):
+          grid[xx][curr_iy] = 1
+      elif curr_iy != prev_iy:
+        for yy in arange(prev_iy, curr_iy):
+          grid[curr_ix][yy] = 1
+      prev_ix = curr_ix
+      prev_iy = curr_iy
+    
+    def check(ax: int, ay: int, bx: int, by: int) -> bool:
+      a_ix = xs.index(ax)
+      a_iy = ys.index(ay)
+      b_ix = xs.index(bx)
+      b_iy = ys.index(by)
+      for ix in arange(a_ix, b_ix):
+        for iy in arange(a_iy, b_iy):
+          if grid[ix][iy] == 0:
+            return False
+      return True
+    
     best = 0
     for i,(ax, ay) in enumerate(tiles):
       for bx, by in tiles[i+1:]:
         if ax == bx and ay == by:
           continue
-        best = max(best, check(grid, ax, ay, bx, by))
+        if check(ax, ay, bx, by):
+          best = max(best, area(ax, ay, bx, by))
     return best
 
 def main():
